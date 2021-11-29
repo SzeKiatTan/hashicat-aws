210c8e5146666c71ba95108bf02163b77ce7d7a9
SzeKiatTan <sze.kiat.tan@gmail.com>
2021-11-29 07:58:13 +0000

    Added s3 bucket module 4

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -2,7 +2,7 @@ module "s3-bucket" {
   source  = "app.terraform.io/SKT-training/s3-bucket/aws"
   version = "2.2.0"
   
-  bucket_prefix = "SKT"
+  bucket_prefix = "skt"
   acl    = "private"
 
   versioning = {
